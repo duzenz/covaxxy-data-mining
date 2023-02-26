e96a90d39375f3a248010cfdcd264019c070e802
duzenz <duzenz@gmail.com>
2023-02-27 01:17:37 +0300

    refactored daily csv creation

--- create_daily_csv.py
+++ create_daily_csv.py
@@ -1,19 +1,22 @@
 import csv
 import json
 import os
+from urllib.parse import urlparse
 
-FILE_NAME = "tweet_ids--2021-03-01"
+FILE_NAME = "tweet_ids--2021-03-02"
 DOWNLOADS_FOLDER = "C:/Users/test.test/Desktop/covaxxy/downloads/" + FILE_NAME
-LONG_FORMAT = False
+LONG_FORMAT = True
+
 
 def create_csv_for_day():
     lines = read_uncredible_sources()
-    #removed text
-    header_long = ['id', 'created_at', 'tweet_id', 'credible', 'author_id', 'len', 'name', 'username', 'verified',
-              'followers_count', 'following_count', 'tweet_count',
-              'retweet_author_id', 'retweet_id', 'retweeted_screen_name',
-              'user_mentions_id', 'user_mentions_screen_name',
-              'in_reply_to_user_id', 'in_reply_to_tweet_id', 'in_reply_to_username']
+    header_long = ['id', 'created_at', 'tweet_id', 'credible', 'author_id',
+                   'text', 'urls', 'name', 'username', 'verified', 'location',
+                   'followers_count', 'following_count', 'tweet_count',
+                   'like_count', 'quote_count', 'reply_count', 'retweet_count',
+                   'retweet_author_id', 'retweet_id', 'retweeted_screen_name',
+                   'user_mentions_id', 'user_mentions_screen_name',
+                   'in_reply_to_user_id', 'in_reply_to_tweet_id', 'in_reply_to_username']
     header_short = ['id', 'created_at', 'tweet_id', 'credible', 'author_id']
     title = FILE_NAME + "-short"
     if LONG_FORMAT is True:
@@ -33,7 +36,8 @@ def create_csv_for_day():
             counter = 0
             if "data" in data:
                 for tweet in data["data"]:
-                    writer.writerow(get_row(tweet, counter, data["includes"]["users"], data["includes"]["tweets"], lines))
+                    writer.writerow(
+                        get_row(tweet, counter, data["includes"]["users"], data["includes"]["tweets"], lines))
                     counter = counter + 1
     f.close()
 
@@ -44,10 +48,12 @@ def set_user_information(tweet, users, row):
         row.append(user_info[0]["name"])
         row.append(user_info[0]["username"])
         row.append(user_info[0]["verified"])
+        row.append(user_info[0]["location"] if "location" in user_info[0] else "#")
         row.append(user_info[0]["public_metrics"]["followers_count"])
         row.append(user_info[0]["public_metrics"]["following_count"])
         row.append(user_info[0]["public_metrics"]["tweet_count"])
     else:
+        row.append("#")
         row.append("#")
         row.append("#")
         row.append("#")
@@ -113,6 +119,13 @@ def set_replied_to_information(tweet, users, row):
         row.append("#")
 
 
+def set_reaction_information(tweet, row):
+    row.append(tweet["public_metrics"]["like_count"])
+    row.append(tweet["public_metrics"]["quote_count"])
+    row.append(tweet["public_metrics"]["reply_count"])
+    row.append(tweet["public_metrics"]["retweet_count"])
+
+
 def get_row(tweet, counter, users, tweets, lines):
     row = []
     row.append(counter)
@@ -121,17 +134,33 @@ def get_row(tweet, counter, users, tweets, lines):
     row.append(get_label(tweet, lines))
     row.append(tweet["author_id"])
     if LONG_FORMAT is True:
-        #row.append(tweet["text"])
-        row.append(len(tweet["text"]))
+        row.append(tweet["text"])
+        set_url_information(tweet, row)
         set_user_information(tweet, users, row)
+        set_reaction_information(tweet, row)
         set_retweet_information(tweet, users, tweets, row)
         set_mention_information(tweet, row)
         set_replied_to_information(tweet, users, row)
     return row
 
 
+def set_url_information(tweet, row):
+    if "entities" in tweet and "urls" in tweet["entities"]:
+        display_url = ""
+        for item in tweet["entities"]['urls']:
+            display_url += urlparse(item['expanded_url']).netloc + ","
+        row.append(display_url)
+    else:
+        row.append("#")
+
+
 def get_label(tweet, lines):
-    res = [ele for ele in lines if (ele in tweet["text"])]
+    display_url = ""
+    if "entities" in tweet and "urls" in tweet["entities"]:
+        for item in tweet["entities"]['urls']:
+            display_url += urlparse(item['expanded_url']).netloc + ","
+
+    res = [ele for ele in lines if (ele in tweet["text"] or ele in display_url)]
     if bool(res):
         return 0
     else:
